991dfa6f8cd4195038ae2872ad6605d77d9588a5
PacoPGD <pakiyo_pgd@hotmail.com>
2014-04-03 16:40:22 +0200

    Añadido main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <GL/glut.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <time.h>
+//g++ -o ejecutable ejemplo13.cpp -lGL -lglut -lGLU
+
+int main(int argcp, char **argv){
+
+}
