8b87fd215d7f5019f3cbec94ac2478d44e9512d0
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2014-05-03 13:34:50 +0200

    Update main.cpp
    Perceptiva bien puesta
    Ejemplo de rotar un cubo
    Salir del programa con esc

--- main.cpp
+++ main.cpp
@@ -104,6 +104,9 @@ void init(){
   pos.set(20,20,0);
   limit.set(0,0,0);
   j.set(4,1,pos,limit);
+  /******************************/
+  //Rotando codo  
+  /*******************************/
   Vector3f rot(45,45,45);
   j.rotate(rot);
   joins.push_back(j);
@@ -166,41 +169,67 @@ void move(int id, Vector3f giro){
   
 }
 
+void dibujarEje(){
+  glColor3f(1.0f,0.0f,0.0f);
+  glVertex3f(0.0f,0.0f,0.0f);
+  glVertex3f(100.0f,0.0f,0.0f);
+  
+  glColor3f(0.0f,1.0f,0.0f);
+  glVertex3f(0.0f,0.0f,0.0f);
+  glVertex3f(0.0f,100.0f,0.0f);
+  
+  glColor3f(0.0f,0.0f,1.0f);
+  glVertex3f(0.0f,0.0f,0.0f);
+  glVertex3f(0.0f,0.0f,100.0f);
+}
+
 void DibujaEscena(){
 		glClearColor(1.,1.,1.,1.);
-		glClear(GL_COLOR_BUFFER_BIT);	
-		glColor3f(1.0f,0.0f,0.0f);
-		glPointSize(10.0f);
-    glBegin(GL_POINTS);
-    int currentRoot=0;
+		glClear(GL_COLOR_BUFFER_BIT);
+		int currentRoot=0;
+
+    glMatrixMode(GL_MODELVIEW);
+    glColor3f(1,0,0);
+    glLoadIdentity();
     glPushMatrix();
-		for(size_t i=0; i<joins.size(); ++i){
-		  int newRoot=joins[i].root;
-		  if(newRoot!=currentRoot){
-        glPushMatrix();
-		  }
-      glRotatef(joins[i].angle.x,joins[i].angle.y,joins[i].angle.z,1);
-      glVertex3f(joins[i].pos.x,joins[i].pos.y, joins[i].pos.z);
-      if(newRoot!=currentRoot){
-        glPopMatrix();
-        currentRoot=newRoot;
-		  }
-		}
+    glScalef(5.0f,1.0f,1.0f);
+    
+    glRotatef(45.0f,0,0,1);
+    glTranslatef(10.0f,0,0);
+    
+    glutWireCube(10.0);
+    glPopMatrix();
+
+    glPointSize(10.0f);
+    glBegin(GL_POINTS);
+    glPushMatrix();
+			for(size_t i=0; i<joins.size(); ++i){
+				int newRoot=joins[i].root;
+				if(newRoot!=currentRoot){
+		      glPushMatrix();
+				}
+				//glTranslatef(0,0,0);
+		    //glRotatef(45, 0, 0,1.0f);
+		    //glTranslatef(0,0,0);
+		    glVertex3f(joins[i].pos.x,joins[i].pos.y, joins[i].pos.z);
+		    if(newRoot!=currentRoot){
+		      glPopMatrix();
+		      currentRoot=newRoot;
+				}
+			}
 		glPopMatrix();
     glEnd();
     
     glBegin(GL_LINES);
-		for(size_t i=1; i<joins.size(); ++i){
-      glVertex3f(joins[i].pos.x,joins[i].pos.y, joins[i].pos.z);
-      int r=joins[i].root;
-      glVertex3f(joins[r].pos.x,joins[r].pos.y, joins[r].pos.z);
-		}
+			for(size_t i=1; i<joins.size(); ++i){
+		    glVertex3f(joins[i].pos.x,joins[i].pos.y, joins[i].pos.z);
+		    int r=joins[i].root;
+		    glVertex3f(joins[r].pos.x,joins[r].pos.y, joins[r].pos.z);
+			}
     glEnd();
     
 		glutSwapBuffers();
 		glFinish();
-		// para de contar el tiempo aqui
-		//glFlush();
 }
 
 void EscalaVentana(GLsizei w, GLsizei h){
@@ -209,9 +238,30 @@ void EscalaVentana(GLsizei w, GLsizei h){
 	hsize=h;
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-100.0f, 100.0f, -100.0f, 100.0f, -50.0, 50.0);
+	GLdouble fovy=60;
+	GLdouble aspect=1.0;
+	GLdouble  zNear=100.0;
+	GLdouble  zFar=500.0;
+	gluPerspective(  fovy,    aspect,    zNear,    zFar);
+	GLdouble eyeX=0;
+	GLdouble eyeY=0;
+	GLdouble eyeZ=-200.0f;
+	GLdouble centerX=0;
+	GLdouble centerY=0;
+	GLdouble centerZ=0;
+	GLdouble upX=0;
+	GLdouble upY=1.0f;
+	GLdouble upZ=0;
+	gluLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
+	//glOrtho(-100.0f, 100.0f, -100.0f, 100.0f, -50.0, 50.0);
 }
-
+void myKeyboard(unsigned char key,int x, int y){
+	switch(key){
+		case 27:
+			exit(0);
+			break;
+	}
+}
 
 int main(int argcp, char **argv){
   init();
@@ -222,7 +272,7 @@ int main(int argcp, char **argv){
 	glutCreateWindow("Esqueleto");
 	glutDisplayFunc(DibujaEscena);
 	glutReshapeFunc(EscalaVentana);
-	
+	glutKeyboardFunc(myKeyboard);
 	/*glutIdleFunc(MueveCuadrado);
 	glutKeyboardFunc(myKeyboard);
 	glutSpecialFunc(myKeyboardSpecial);
